36cc0ff08a008620503354f7975d32e8edbdaaf3
Julian Currie <julian.currie@gmail.com>
2017-02-16 09:01:10 +0000

    add camp ground names and images to new page. link from landing

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require("express");
 var app = express();
 
+
+
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res) {
     res.render("landing");
+});
+
+app.get('/campgrounds', function(req, res) {
+    var campgrounds = [ 
+        {name: "Salmon Creak", image: "http://cdn-image.travelandleisure.com/sites/default/files/styles/720x450/public/1443561122/CAMPING0915-Glacier-National-Park.jpg"},
+        {name: "Marble Hill", image: "http://i.telegraph.co.uk/multimedia/archive/01653/trericket_1653323c.jpg"},
+        {name: "Rocky Road", image: "http://esq.h-cdn.co/assets/15/28/980x653/gallery-1436200680-10-jumbo-rocks.jpg"}
+        ]
+        
+        res.render("campgrounds", {campgrounds, campgrounds});
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
