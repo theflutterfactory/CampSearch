88e02dbd8100d926437596048121fd98a8d23d24
Julian Currie <julian.currie@gmail.com>
2017-02-16 09:53:07 +0000

    added body-parser pkg and New campground post view

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get('/', function(req, res) {
@@ -17,6 +17,14 @@ app.get('/campgrounds', function(req, res) {
         ]
         
         res.render("campgrounds", {campgrounds, campgrounds});
+});
+
+app.post('/campgrounds', function(req, res) {
+    res.send("you hit the post route");
+});
+
+app.get('/campgrounds/new', function(req, res) {
+   res.render("new.ejs"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
