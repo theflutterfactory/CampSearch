f4a7d09f8da612737e1cd0982d617da0a46825b5
Julian Currie <julian.currie@gmail.com>
2017-02-19 11:16:50 +0000

    fixed name key, created comment model

--- seeds.js
+++ seeds.js
@@ -4,17 +4,17 @@ var Comment = require("./models/comment");
 
 var data = [
     {
-        namme: "Pikachu Park",
+        name: "Pikachu Park",
         image: "http://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
         description: "Electrifying greenery"
     },
     {
-        namme: "Rattata Ridge",
+        name: "Rattata Ridge",
         image: "https://lh3.googleusercontent.com/-NQIWoCfR6Fk/Vy1a_-v6XPI/AAAAAAAAGSY/IJgz_KJ18EorX4M_fCyQgT6cJV00wesuACCo/s300/Screen%2Bshot%2B2016-05-06%2Bat%2B10.53.36%2BPM.png?refresh=900&resize_h=NaN&resize_w=NaN",
         description: "A nasty infestation"
     },
     {
-        namme: "Hypno Hill",
+        name: "Hypno Hill",
         image: "http://pokiidex.com/Content/Images/097_hypno.png",
         description: "Rolling hills that will put you in a trance"
     }
@@ -34,7 +34,7 @@ function seedDB() {
                             campground.save();
                             console.log("Created new comment");
                         }).catch(function(err) {
-                            
+                            console.log(err);
                         });
             }).catch(function(err) {
                 console.log(err);
