e3f41b4d167e4834868529217fd71b832daf41b2
Julian Currie <julian.currie@gmail.com>
2017-02-20 08:48:15 +0000

    added Bootstrap styling for show.ejs, removed seed comments

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Pikachu Park",
         image: "http://vignette2.wikia.nocookie.net/pokemon/images/b/b1/025Pikachu_XY_anime_3.png/revision/latest?cb=20140902050035",
-        description: "Electrifying greenery"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Rattata Ridge",
         image: "https://lh3.googleusercontent.com/-NQIWoCfR6Fk/Vy1a_-v6XPI/AAAAAAAAGSY/IJgz_KJ18EorX4M_fCyQgT6cJV00wesuACCo/s300/Screen%2Bshot%2B2016-05-06%2Bat%2B10.53.36%2BPM.png?refresh=900&resize_h=NaN&resize_w=NaN",
-        description: "A nasty infestation"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     },
     {
         name: "Hypno Hill",
         image: "http://pokiidex.com/Content/Images/097_hypno.png",
-        description: "Rolling hills that will put you in a trance"
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 ]
 
@@ -26,21 +26,6 @@ function seedDB() {
         
         data.forEach(function(seed) {
             console.log("Added new Campground!");
-
-            Campground.create(seed).then(function(campground) {
-                Comment.create({
-                        text: "This place is greate, but I wish there was internet",
-                        author: "Einstein"
-                        }).then(function(comment) {
-                            campground.comments.push(comment);
-                            campground.save();
-                            console.log("Created new comment");
-                        }).catch(function(err) {
-                            console.log(err);
-                        });
-            }).catch(function(err) {
-                console.log(err);
-            });
         });
     }).catch(function(err) {
         console.log(err); 
