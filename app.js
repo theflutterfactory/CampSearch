dbbe7f963b174075424f54b26609730619d67fdc
Julian Currie <julian.currie@gmail.com>
2017-02-20 09:11:31 +0000

    added main.css file with thumbnail with and desc padding

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ seedDB();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
     res.render("landing");
