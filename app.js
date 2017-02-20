27b8ae544320b33c4b2b773ba14b5fb58ad3f90b
Julian Currie <julian.currie@gmail.com>
2017-02-20 11:30:23 +0000

    more passport setup for the user object

--- app.js
+++ app.js
@@ -13,11 +13,23 @@ var LocalStrategy = require("passport-local");
 mongoose.Promise = require('bluebird');
 mongoose.connect("mongodb://localhost/yelp_camp");
 
-seedDB();
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+seedDB();
+
+app.use(require("express-session")({
+    secret: "Pikachu is highly overrated",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res) {
     res.render("landing");
