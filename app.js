9a5c51504f7598b8b95425d4b78d004d174cf1a3
Julian Currie <julian.currie@gmail.com>
2017-02-20 12:46:08 +0000

    added login view, added login authentication

--- app.js
+++ app.js
@@ -112,6 +112,17 @@ app.post('/register', function(req, res) {
         });
     });
 });
+
+app.get('/login', function(req, res) {
+    res.render("login");
+});
+
+app.post('/login', passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
     
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("YelpCamp server started");
