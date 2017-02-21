e289dc8232fed150d0f19324a4b627b9c4184061
Julian Currie <julian.currie@gmail.com>
2017-02-21 13:16:58 +0000

    added commit edit button, commit edit view and commit edit GET/PUT

--- routes/comments.js
+++ routes/comments.js
@@ -30,6 +30,24 @@ router.post('/', isLoggedIn, function(req, res) {
     });
 });
 
+router.get('/:comment_id/edit', function(req, res) {
+    Comment.findById(req.params.comment_id).then(function(foundComment) {
+        res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }).catch(function(err) {
+        console.log(err);
+        res.redirect("back");
+    });
+});
+
+router.put('/:comment_id', function(req, res) {
+   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment).then(function(updatedComment) {
+       res.redirect("/campgrounds/" + req.params.id);
+   }).catch(function(err) {
+        console.log(err);
+        res.redirect("back");
+   });
+});
+
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
         return next();
