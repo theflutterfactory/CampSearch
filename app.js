8d18276218004799a3701f54447a955ce7ee40a0
Julian Currie <julian.currie@gmail.com>
2017-02-21 08:28:18 +0000

    updated commentSchema, associate User w/ Comment, removed Author input, show author w/ comment

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-seedDB();
-
 app.use(require("express-session")({
     secret: "Pikachu is highly overrated",
     resave: false,
--- routes/comments.js
+++ routes/comments.js
@@ -14,6 +14,10 @@ router.get('/new', isLoggedIn, function(req, res) {
 router.post('/', isLoggedIn, function(req, res) {
     Campground.findById(req.params.id).then(function(campground) {
         Comment.create(req.body.comment).then(function(comment) {
+            comment.author.id = req.user._id;
+            comment.author.username = req.user.username;
+            comment.save();
+            
             campground.comments.push(comment);
             campground.save();
             res.redirect('/campgrounds/' + campground._id);
