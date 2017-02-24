64b64af5e89f5c15428a3608121597012eaecf51
Julian Currie <julian.currie@gmail.com>
2017-02-24 10:18:33 +0000

    added login failure flash msg and changed landing button type

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,8 @@ router.get('/login', function(req, res) {
 router.post('/login', passport.authenticate("local", 
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login"
+        failureRedirect: "/login",
+        failureFlash: true,
     }), function(req, res) {
 });
 
